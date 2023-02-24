ff841083c50c2490495b686eba8a6cef8fdac093
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-02-24 08:17:38 -0300

    Update q5.py

--- atividades/exercicio01/q5.py
+++ atividades/exercicio01/q5.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 navegador = webdriver.Chrome()
+navegador.get('https://www.google.com.br')
 
-navegador.get('http://www.google.com/search.')
+sleep(1)
 
-elemento = navegador.find_element(by='tag_name', value='input')
+elemento = navegador.find_element(by='name', value='q').send_keys('q' + Keys.RETURN)
 
-elemento.send_keys('q')
+sleep(2)
+
+page_content = navegador.page_source
+
+site = BeautifulSoup(page_content, 'html.parser')
+
+print(site.prettify())
