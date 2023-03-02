ba3b3fd8d586a9104e69fd587a930103e655d40f
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-03-02 19:27:24 -0300

    Update atv.py

--- atividades/exercicio02/atv.py
+++ atividades/exercicio02/atv.py
@@ -37,47 +37,23 @@ class CacheRequest():
     
 
 c = CacheRequest (life=60)
-lista_prod = []
+c = CacheRequest (life=60)
 url = 'https://lista.mercadolivre.com.br/'
-produto_nome = 'Musica'
-if c.get(url + produto_nome):
-        
-        for page in range(1,3):
-            site =  site = BeautifulSoup(c.text, 'html.parser')
-            produtos = site.find_all('div', {'class': 'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core ui-search-result--advertisement andes-card--padding-default'})
+produto_nome = 'fone de ouvido'
+productlink = []
+for x in range(1,6):
+    if c.get(url + produto_nome):
+        site = BeautifulSoup(c.text,'html.parser')
 
-            for produto in produtos:
-                titulo = produto.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
-                real = produto.find('span', attrs={'class': 'price-tag-fraction'})
-                cent = produto.find('span', attrs={'class': 'price-tag-cents'})
-                link = produto.find('a', attrs={'class': 'ui-search-link'})
+        productlist = site.find_all('div', {'class': 'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core ui-search-result--advertisement andes-card--padding-default'})
 
-                #print(produto.prettify())
-                #print('Titulo: ', titulo.text)
-                #print('Link: ', link['href'])
+        for item in productlist:
+            titulo = item.find('h2', attrs={'class': 'ui-search-item__title shops__item-title'})
 
-                
-                texto = site.find(produto_nome in titulo).get_text().strip()
-                print('\20',texto, '\20')
-                    
-
-                if(cent):
-                    #print('Preço: ', real.text + ',' + cent.text)
-                    lista_prod.append([titulo.text, link['href'], real.text,cent.text])
-                else:
-                    #print('Preço: ', real.text)
-                    lista_prod.append([titulo.text, link['href'], real.text, ''])
-
-                print('\n\n')
-
-            next_li_element = site.find('li', class_='next')
-
-            while(next_li_element is not None):
-                next_page = next_li_element.find('a', href=True)['href']
-                page = requests.get(url + next_page)
-                site = BeautifulSoup(page.text, 'html.parser')
+            for link in item.find_all('a',href=True):
+                productlink.append([titulo.text,link['href']])
 
 
-total = pd.DataFrame(lista_prod, columns=['Titulo','Link','Real','Centavos'])
-total.to_excel('produtos.xlsx', index=False)
-print(total)
+prod = pd.DataFrame(productlink, columns=['Titulo', 'Link'])
+prod.to_excel('prod.xlsx', index=False)
+print(prod)
