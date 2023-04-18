1ebb3add0dd9b68a578fef511edf95a5bab3c32e
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-04-18 15:24:25 -0300

    Create script3.js

---
+++ atividades/exercicio06/questao06/script3.js
@@ -0,0 +1,13 @@
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+function rodar1() {
+    changeColor('black');
+    
+} 
+
+function rodar2() {
+    changeColor('white');
+    
+} 
